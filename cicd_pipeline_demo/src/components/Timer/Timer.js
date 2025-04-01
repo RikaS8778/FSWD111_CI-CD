da846ad70bfc948a17b3616545aa717ffb09950b
rika.s <rika.s8778@gmail.com>
2025-03-31 20:50:35 -0700

    update timer component

--- cicd_pipeline_demo/src/components/Timer/Timer.js
+++ cicd_pipeline_demo/src/components/Timer/Timer.js
@@ -1,9 +1,31 @@
 import './Timer.css';
+import { useState, useEffect } from 'react';
 
 const Timer = () => {
+
+    const getTime = () => {
+        const date = new Date();
+        const h = date.getHours();
+        const m = date.getMinutes();
+        const s = date.getSeconds();
+        return `${h < 10 ? '0' + h : h}:${m < 10 ? '0' + m : m}:${s < 10 ? '0' + s : s}`
+    }
+
+    const [times, setTimes] = useState(getTime);
+
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTimes(getTime)
+        }, 1000);
+
+        // Cleanup interval on component unmount
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
         <div className='Timer'>
-            Hey! from Timer
+            <p>{times}</p>
         </div>
     )
 }
